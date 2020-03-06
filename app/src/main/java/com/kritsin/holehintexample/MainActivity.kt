d5284a71692ee7ed1253fed043f3b4732e9c0b9d
Aleksandr Kritsin <kritsinalexandr@gmail.com>
2020-03-07 00:47:32 +0300

    init

---
+++ app/src/main/java/com/kritsin/holehintexample/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.kritsin.holehintexample
+
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.kritsin.holehint.HoleHintFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
+        Handler().postDelayed({
+            HoleHintFragment.showDialog(
+                supportFragmentManager,
+                navView.findViewById<BottomNavigationItemView>(R.id.navigation_dashboard)
+            )
+        }
+            , 3000
+        )
+
+    }
+}
